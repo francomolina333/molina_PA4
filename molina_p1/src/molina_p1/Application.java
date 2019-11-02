e7d5a50c508756fc551b204fbde341bc3c030662
Franco Molina <francomolina333@gmail.com>
2019-11-02 17:08:49 -0400

    initial commit

---
+++ molina_p1/src/molina_p1/Application.java
@@ -0,0 +1,13 @@
+package molina_p1;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// 2 pts
+		
+		DuplicateRemover duplicateRemover = new DuplicateRemover();
+		//2 pts
+
+	}
+
+}
---
+++ molina_p1/src/molina_p1/DuplicateRemover.java
@@ -0,0 +1,16 @@
+package molina_p1;
+
+public class DuplicateRemover {
+	
+	public static void remove() {
+		//2pts
+	}
+	public static void write() {
+		//2pts
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
