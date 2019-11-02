4ef1b5ef9601b77ea70097540f0da7e243923eb5
Franco Molina <francomolina333@gmail.com>
2019-11-02 17:12:43 -0400

    Inital commit

---
+++ molina_p2/src/molina_p2/Application.java
@@ -0,0 +1,12 @@
+package molina_p2;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// 2 pts
+		DuplicateCounter duplicateCounter = new DuplicateCounter();
+		//2 pts
+
+	}
+
+}
---
+++ molina_p2/src/molina_p2/DuplicateCounter.java
@@ -0,0 +1,13 @@
+package molina_p2;
+
+public class DuplicateCounter {
+	public static void count() {
+		// 2pts
+	}
+	public static void write() {
+		// 2pts
+	}
+	public static void main(String[] args) {
+		
+	}
+}
